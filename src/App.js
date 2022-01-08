24b483d492745d7f3a093d6f002b366e7c916213
Peter Tobbell <info@neocad.co.uk>
2022-01-08 08:39:01 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Header text={true}/>
+    <div className='container'>
+      <h1> My App</h1>
+
+    </div>
+    
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+
+export default function Header({text}) {
+    return (
+        <header>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+            
+        </header>
+    )
+
+    Header.defaultProps = {text: 'Feedback UI',
+    Header.prototype = {
+        text: PropTypes.string
+    
+}
+
+
+}
+
+
+}
+
+
